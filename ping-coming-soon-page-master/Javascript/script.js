8c81e42ba671a50b492a5ca030d27717ab764c54
Lilee52 <lilianbalogun52@gmail.com>
2022-09-26 11:32:45 +0530

    projrct done

---
+++ ping-coming-soon-page-master/Javascript/script.js
@@ -0,0 +1,24 @@
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+
+form.addEventListener("submit", (e) => {
+    let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_'{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let messages = []
+    if (email.value === "" || email.value == null) {
+        messages.push("Whoops! It looks like you forgot to add your email");
+        email.style.border = "2px solid hsl(354, 100%, 66%)";
+    }
+
+    if (email.value !== validEmail || email.value !== "") {
+        messages.push("Please provide a valid email address");
+        email.style.border = "2px solid hsl(354, 100%, 66%)";
+        errorElement.style.marginBottom = "2em";
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(", ");
+    }
+});
